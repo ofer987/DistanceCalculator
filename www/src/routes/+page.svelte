78cf9cfeec85043744cfc1d65a8ee39441ba7b99
Dan Jakob Ofer <dan@ofer.to>
2024-03-11 11:02:18 -0400

    chore(loading): add animation

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -27,43 +27,110 @@
 	}
 </script>
 
-<div>
-	{#if geolocationCoordinatesFound == null}
-		<div>Loading</div>
-	{:else if !geolocationCoordinatesFound || !latitude || !longitude}
-		<div>{errorMessage}</div>
-	{:else}
-		{#await getTimeTable(latitude, longitude)}
-			<div>Loading</div>
-		{:then lines}
-			<div class="timetable">
-				{#each lines as line}
-					<div
-						class="line"
-						class:subway-type={line.lineType == 'Subway'}
-						class:streetcar-type={line.lineType == 'Streetcar'}
-						class:bus-type={line.lineType == 'Bus'}
-						class:disabled={!line.isAvailable}
-					>
-						<div id="agency-name">{line.agencyName}</div>
-						<div id="type">{line.lineType}</div>
-						<div id="id">{line.id}</div>
+{#if geolocationCoordinatesFound == null}
+	<div class="centre loading">
+		<div class="text">Loading</div>
+		<div class="loading-item" id="first" />
+		<div class="loading-item" id="second" />
+		<div class="loading-item" id="third" />
+	</div>
+{:else if !geolocationCoordinatesFound || !latitude || !longitude}
+	<div class="centre error">
+		<div class="text">{errorMessage}</div>
+	</div>
+{:else}
+	{#await getTimeTable(latitude, longitude)}
+		<div class="centre loading">
+			<div class="text">Loading</div>
+			<div class="loading-item" id="first" />
+			<div class="loading-item" id="second" />
+			<div class="loading-item" id="third" />
+		</div>
+	{:then lines}
+		<div class="timetable">
+			{#each lines as line}
+				<div
+					class="line"
+					class:subway-type={line.lineType == 'Subway'}
+					class:streetcar-type={line.lineType == 'Streetcar'}
+					class:bus-type={line.lineType == 'Bus'}
+					class:disabled={!line.isAvailable}
+				>
+					<div id="agency-name">{line.agencyName}</div>
+					<div id="type">{line.lineType}</div>
+					<div id="id">{line.id}</div>
 
-						<div class="directions">
-							{#each line.directions as direction}
-								<Line {direction} />
-							{/each}
-						</div>
+					<div class="directions">
+						{#each line.directions as direction}
+							<Line {direction} />
+						{/each}
 					</div>
-				{/each}
-			</div>
-		{:catch error}
-			<div>Oops {error}</div>
-		{/await}
-	{/if}
-</div>
+				</div>
+			{/each}
+		</div>
+	{:catch}
+		<div class="centre error">
+			<div class="text">Unable to reach server</div>
+		</div>
+	{/await}
+{/if}
 
 <style lang="scss">
+	.centre {
+		position: fixed;
+		top: 50%;
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+
+		.text {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+		}
+
+		.loading-item {
+			width: 2em;
+			height: 2em;
+			color: black;
+			background-color: blue;
+			margin: 0 1em;
+
+			animation-play-state: running;
+			animation-fill-mode: none;
+			animation-name: blink;
+			animation-duration: 1s;
+			animation-iteration-count: infinite;
+
+			@keyframes blink {
+				0% {
+					opacity: 0;
+				}
+
+				50% {
+					opacity: 1;
+				}
+
+				100% {
+					opacity: 0;
+				}
+			}
+
+			&#first {
+				animation-timing-function: steps(4, jump-end);
+			}
+
+			&#second {
+				animation-timing-function: steps(2, jump-end);
+			}
+
+			&#third {
+				animation-timing-function: steps(1, jump-end);
+			}
+		}
+	}
+
 	.timetable {
 		display: flex;
 		flex-direction: column;
